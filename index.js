28acbaf57160cff36dc2453f09c3cd25224fc637
lewis-otwori <lewisotwori@gmail.com>
2023-03-23 16:09:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+//document.addEventListener("DOMContentLoaded", function() {
+
+  //  let par = document.getElementById("text")
+//console.log(par);
+//document.par.textContent="This is really cool!"
+  //  console.log("The DOM has loaded");
+  //});
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
